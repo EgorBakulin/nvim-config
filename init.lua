2612188b64cba8ea0f00d3904387b80d7c8e4f4e
EgorBakulin <egorbakulin@yahoo.com>
2022-07-20 00:45:24 +0700

    reworked init to lua

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- Plugins
+vim.api.nvim_exec('source ~/.config/nvim/src/plugins.vim', false)
+
+-- appearance
+vim.api.nvim_exec('source ~/.config/nvim/src/appearance.vim', false)
+
+-- tabulation
+vim.api.nvim_exec('source ~/.config/nvim/src/tabulation.vim', false)
+
+-- search
+vim.api.nvim_exec('source ~/.config/nvim/src/search.vim', false)
+
+-- folding
+vim.api.nvim_exec('source ~/.config/nvim/src/folding.vim', false)
