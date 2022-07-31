0c12b2471122c45070281d4c5f555b48f8b857af
ebakulin <egor.bakulin@noveogroup.com>
2022-07-31 23:34:03 +0700

    setup new plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,16 +8,51 @@ require('packer').startup(function(use)
     use 'rafcamlet/coc-nvim-lua'
 
     use 'mattn/emmet-vim'
-    use 'Yggdroot/indentLine'
-    use 'preservim/nerdtree'
-    use 'xuyuanp/nerdtree-git-plugin'
+
+    use 'kyazdani42/nvim-web-devicons'
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+        config = function() 
+            require('lualine').setup {
+                options = {
+                    theme = 'gruvbox-material',
+                    component_separators = { left = '', right = ''},
+                    section_separators = { left = '', right = ''}
+                }
+            }
+            require('lualine').setup()
+        end
+    }
+
+    use {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        requires = { 
+            "nvim-lua/plenary.nvim",
+            "kyazdani42/nvim-web-devicons", 
+            "MunifTanjim/nui.nvim",
+        },
+        config = function()
+            vim.api.nvim_set_keymap('n', '<C-n>', ':NeoTreeShowToggle<CR>', { noremap = true })
+        end
+    }
+
     use 'racer-rust/vim-racer'
+
     use 'godlygeek/tabular'
+
     use 'flazz/vim-colorschemes'
+
     use 'easymotion/vim-easymotion'
+
     use 'airblade/vim-gitgutter'
+
     use 'stephpy/vim-php-cs-fixer'
+
     use 'mhinz/vim-signify'
+
     use 'qbbr/vim-twig'
 end)
 
@@ -26,7 +61,6 @@ let g:coc_disable_startup_warning = 1
 
 let g:coc_global_extensions = ['coc-git', 'coc-phpls', 'coc-css', 'coc-sh', 'coc-rome', 'coc-clangd']
 
-map <C-n> :NERDTreeToggleVCS<CR>
 
 nmap s <Plug>(easymotion-w)
 nmap S <Plug>(easymotion-b)
