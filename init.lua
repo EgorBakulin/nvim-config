ad86fb25d85f4db50b3716873bbcca1320e62b3b
ebakulin <egor.bakulin@noveogroup.com>
2022-11-30 20:13:10 +0600

    added autocmd for markdown files

--- init.lua
+++ init.lua
@@ -1,17 +1,13 @@
--- Plugins
 require('plugins')
 
--- LSP
 require('lsp')
 
--- appearance
 require('appearance')
 
--- tabulation
 require('tabulation')
 
--- search
 require('search')
 
--- folding
 require('folding')
+
+require('autocmd')
